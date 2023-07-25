3b8585209378a5ab83ab280fd59f4e263330dd51
0xAnon <utkarshshail005@protonmail.com>
2023-07-25 19:32:21 +0530

    More improved Version

--- src/App.js
+++ src/App.js
@@ -1,51 +1,13 @@
 import { Main } from './Main';
-
-import '@rainbow-me/rainbowkit/styles.css';
 import { ChakraProvider } from '@chakra-ui/react'
+import { ToastContainer} from 'react-toastify';
 
-import {
-  getDefaultWallets,
-  RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import {
-  mainnet,
-  polygon,
-  optimism,
-  arbitrum,
-  zora,
-} from 'wagmi/chains';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
-
-const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, zora],
-  [
-    alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
-    publicProvider()
-  ]
-);
-
-const { connectors } = getDefaultWallets({
-  appName: 'My RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
-  chains
-});
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient
-})
 
 const App = () => {
   return (
     <ChakraProvider>
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
         <Main />
-      </RainbowKitProvider>
-    </WagmiConfig>
+        <ToastContainer />
     </ChakraProvider>
 
   );
--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,112 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { Text , Textarea , Button} from '@chakra-ui/react';
+import { Text , Textarea , Button, Spinner} from '@chakra-ui/react';
 import React, { useState } from 'react';
 import Papa from "papaparse";
+import fromExponential from 'from-exponential';
+import {ethers} from 'ethers'
 import './Main.css'
+import {toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import web3 from 'web3'
 
 export const Main = () => {
+
 
 	const [mainValue, setValue ] = useState("")
 	const [inputError,setInputError]=useState("")
   const [file, setFile] = useState("");
+  const [isSending,setIsSending]= useState(false)
+
+  const contract = {
+    "abi":[{
+      "constant": false,
+      "inputs": [
+        {
+          "name": "recipients",
+          "type": "address[]"
+        },
+        {
+          "name": "values",
+          "type": "uint256[]"
+        }
+      ],
+      "name": "airdropNative",
+      "outputs": [],
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "function"
+    }],
+    "address":"0xf77ce462b55fc9addf82104adfe0994e61bc2ff7"
+  }
+
+  const { ethereum } = window;
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+
+  const abi = contract.abi
+  const contractAddress = contract.address
+  const chainId = 5
+
+  const [accountAddress, setAccountAddress] = useState('');
+  const [accountBalance, setAccountBalance] = useState('');
+  const [isConnected, setIsConnected] = useState(false);
+
+  const connectWallet = async () => {
+
+    console.log(window.ethereum.networkVersion)
+
+    if (window.ethereum.networkVersion != chainId) {
+      await changeNetwork()
+    }
+
+    try {
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+      let balance = await provider.getBalance(accounts[0]);
+      let bal = ethers.utils.formatEther(balance);
+      setAccountAddress(accounts[0]);
+      setAccountBalance(bal);
+      setIsConnected(true);
+
+      } catch (error) {
+      setIsConnected(false);
+    }
+  };
+
+  const changeNetwork = async () => {
+    try {
+      await window.ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: web3.utils.toHex(chainId) }]
+      });
+    } catch (err) {
+        // This error code indicates that the chain has not been added to MetaMask
+      if (err.code === 4902) {
+        await window.ethereum.request({
+          method: 'wallet_addEthereumChain',
+          params: [
+            {
+              chainName: 'Goerli Testnet',
+              chainId: web3.utils.toHex(chainId),
+              nativeCurrency: { name: 'ETH', decimals: 18, symbol: 'ETH' },
+              rpcUrls: ['https://goerli.etherscan.io/']
+            }
+          ]
+        });
+      }
+    }
+
+    toast.success('Changed network to Goerli Testnet!', {
+      position: "top-center",
+      autoClose: 4000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      });
+  }
 
 
 	const toSimplifyInput = async (inputString) => {
@@ -109,17 +207,139 @@ export const Main = () => {
   };
 
 
+  const searchFunc = async () => {
+
+		var result = toSimplifyInput(mainValue)
+		var Addresses=(await result).allAddress
+		var Values=(await result).allValue
+		var toSend=0
+		var finalValues=[]
+		var finalAddresses=[]
+		var isBalanceGreater=false
+
+		for(var z of Values){
+			toSend+=parseFloat(z)
+			finalValues.push(ethers.BigNumber.from(String(fromExponential(parseFloat(z)*10**18))))
+		}
+
+		toSend = fromExponential((toSend)*10**18)
+    console.log(toSend/10**18,accountBalance)
+
+		if(toSend/10**18>accountBalance){
+			isBalanceGreater=true
+			setInputError('Not enough balance')
+		}
+
+		for(var x of Addresses){
+      try{
+      var tempAdd = ethers.utils.getAddress(x)
+		  }catch(e){
+        setInputError("Not valid addresses provided!")
+      }
+      finalAddresses.push(tempAdd)
+
+	}
+
+		console.log(finalAddresses)
+		console.log(finalValues)
+		console.log(toSend/10**18)
+		return {finalAddresses,finalValues,toSend}
+	}
+
+	const mainFunc = async () => {
+		setInputError('')
+		// setSubmitStatus(true)
+		var results = await searchFunc()
+		var finalAddressesToSend = results.finalAddresses
+		var finalValuesToSend = results.finalValues
+		var _toSend = results.toSend
+    
+
+    if(finalAddressesToSend.length===finalValuesToSend.length && finalAddressesToSend.length!==0 && finalValuesToSend.length!==0 && inputError===''){
+
+
+
+      const provider = new ethers.providers.Web3Provider(ethereum)
+      const signer = provider.getSigner()
+      const tempContract = new ethers.Contract(contractAddress,abi,signer)
+      const tx = await tempContract.airdropNative(finalAddressesToSend,finalValuesToSend,{ value: _toSend })
+      setIsSending(true)
+
+      toast.success('Waiting for the transaction to get confirmed!', {
+        position: "top-center",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+
+
+      try{
+      await tx.wait()	
+      toast.success('Transaction went through successfully!', {
+        position: "top-center",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+      setIsSending(false)
+
+      }catch(e){
+        console.log(e)
+        toast.error('Something Went Wrong!', {
+          position: "top-center",
+          autoClose: 4000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          });
+      setIsSending(false)
+
+      }
+
+    }else{
+      toast.error(inputError, {
+        position: "top-center",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+    }
+
+	}
+
+
   return (
     
     <div className='container'>
+      
 
         <div className='buttonContainer' >
-        
-        <ConnectButton 
-        chainStatus="icon"    
-        showBalance={true} 
-        />
-
+      
+      {isConnected ? (
+            <>
+            <Button className='xyz' colorScheme='green' size='sm' onClick={connectWallet}>Connected as {accountAddress.slice(0, 4)}...{accountAddress.slice(38, 42)}</Button>
+          </>):
+        (
+          <>
+      <Button className='xyz' colorScheme='green' size='sm' onClick={connectWallet}>Connect</Button>
+          </>
+        )
+        }
         </div>
         
         <div  className='main'>
@@ -137,10 +357,20 @@ export const Main = () => {
         bgGradient='linear(to-l, #39FF13, #0D7377)'
         bgColor='black'
         bgClip='text'
-        fontSize='xl'
+        fontSize='3xl'
         fontWeight='bold'
          >
         A dapp to airdrop tokens to multiple wallets
+        </Text>
+
+        <Text
+        bgGradient='linear(to-l, #39FF13, #0D7377)'
+        bgColor='black'
+        bgClip='text'
+        fontSize='s'
+        fontWeight='bold'
+         >
+        Input the addresses and amounts like the given below example
         </Text>
 
         </div>
@@ -148,8 +378,10 @@ export const Main = () => {
         <div>
 
         <div className='textarea'>
+        
         <Textarea 
-        placeholder='Here is a sample placeholder' 
+        placeholder='0xBE4ca24f9c2cEF343e00F50CB99cD0539104BCeD,250
+        0x0e7c2692b8e123b2030515a1d45886f9ac37e84c,500' 
         onChange={e => {
           setValue(e.target.value)
         }}
@@ -162,18 +394,33 @@ export const Main = () => {
 						id="csvInput"
 						name="file"
 						type="File"
-						/>
+				/>
             
+
       <Button className='thirdButton' colorScheme='green' size='sm' onClick={handleParse}>Parse data from file</Button>
 
+
       </div>
 
+      {!isSending ? (
+        <>
+      <Button className='fourthButton' colorScheme='green' size='md' onClick={mainFunc}>Sumbit</Button>
+        </>
+      ):(
+        <>
+        <Spinner
+      className='fourthButton'
+      thickness='4px'
+      speed='0.65s'
+      emptyColor='gray.200'
+      color='#1B9F58'
+      size='xl'
+      />
+        </>
+      )}
 
-
-      
       </div>
 
-        
         </div>
 
     </div>
